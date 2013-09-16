c8e2077a1c6f8a7279a2e12ed5a654c1711db9c2
Weston Ruter <weston@x-team.com>
2013-09-16 00:39:49 -0700

    Add station-specific themes

--- docroot/wp-content/themes/radio/footer.php
+++ docroot/wp-content/themes/radio/footer.php
@@ -15,7 +15,7 @@
 			<?php do_action( 'radio_credits' ); ?>
 			<a href="http://wordpress.org/" title="<?php esc_attr_e( 'A Semantic Personal Publishing Platform', 'radio' ); ?>" rel="generator"><?php printf( __( 'Proudly powered by %s', 'radio' ), 'WordPress' ); ?></a>
 			<span class="sep"> | </span>
-			<?php printf( __( 'Theme: %1$s by %2$s.', 'radio' ), 'radio', '<a href="http://x-team.com/wordpress/" rel="designer">X-Team</a>' ); ?>
+			<?php printf( __( 'Theme: %1$s + %2$s by %3$s.', 'radio' ), get_template(), get_stylesheet(), '<a href="http://x-team.com/wordpress/" rel="designer">X-Team</a>' ); ?>
 		</div><!-- .site-info -->
 	</footer><!-- #colophon -->
 </div><!-- #page -->
